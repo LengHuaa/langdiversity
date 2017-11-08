c355d2d0bf3c9d05c0f19f1ee507bfeb978dbc87
LengHuaa <33490977+LengHuaa@users.noreply.github.com>
2017-11-08 19:30:38 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,26 @@
+{
+  "ClientId": 377823594857627648,
+  "Token": "Mzc3ODIzNTk0ODU3NjI3NjQ4.DOSjRA.hoMU85VuhJqfWcWjd4uNDdR1ayU",
+  "OwnerIds": [
+    339081678586904577
+  ],
+  "LoLApiKey": "",
+  "GoogleApiKey": "AIzaSyDcpP-urUJIBxd_2uafXmceqB497t3O7qI",
+  "MashapeKey": "2CVm2q2auSmshr6PMu8EuKXNMPLZp1f3Q9ajsnUWMcSSbHI5Fp",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": "die",
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null
+}
+client.login(process.env.Mzc3ODIzNTk0ODU3NjI3NjQ4.DOTssA.CXFwu5loIzaXG-mN3IL7K77jcNA);
